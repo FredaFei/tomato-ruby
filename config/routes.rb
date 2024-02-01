ed978bd89a52d04f2f02e796922c6c2f09ba4628
fredafei <964959033@qq.com>
2024-02-01 09:31:56 +0800

    添加session和me的路由

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :validation_codes, only: [:create]
+      resource :session, only: [:create, :destroy]
+      resource :me, only: [:show]
     end
   end
 
