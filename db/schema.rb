8995d84703ce72c62e269a54d51abe03fc176033
fredafei <964959033@qq.com>
2024-02-01 12:51:38 +0800

    tagsId 改为 tagIds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_01_042732) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_01_044954) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_01_042732) do
     t.bigint "user_id"
     t.integer "amount"
     t.text "note"
-    t.bigint "tags_id", array: true
+    t.bigint "tag_ids", array: true
     t.datetime "happened_at"
     t.datetime "deleted_at"
     t.integer "kind", null: false
