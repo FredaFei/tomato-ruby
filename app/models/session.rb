e09e45d155b844b5387c2719cce3701abbcdee2f
fredafei <964959033@qq.com>
2024-05-10 12:38:30 +0800

    记录验证码被使用时间

--- app/models/session.rb
+++ app/models/session.rb
@@ -9,8 +9,13 @@ class Session
   def check_validation_code
     return if self.code.empty?
     return if Rails.env.development? && self.code == '123456'
-    self.errors.add :email, :not_found unless
-      ValidationCode.exists? email: self.email, code: self.code, used_at: nil
+    
+    validation_code = ValidationCode.find_by(email: self.email, code: self.code, used_at: nil)
+    if validation_code
+      validation_code.update(used_at: Time.now)
+    else
+      self.errors.add :email, :not_found
+    end
   end
 
 end
