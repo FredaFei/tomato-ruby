2a2abe5ba4f7823eeb566ffb21e73e83a054427b
fredafei <964959033@qq.com>
2024-05-10 12:30:41 +0800

    仅在本地开发环境中验证码可以是123456

--- app/models/session.rb
+++ app/models/session.rb
@@ -8,7 +8,7 @@ class Session
 
   def check_validation_code
     return if self.code.empty?
-    return if self.code == '123456'
+    return if Rails.env.development? && self.code == '123456'
     self.errors.add :email, :not_found unless
       ValidationCode.exists? email: self.email, code: self.code, used_at: nil
   end
